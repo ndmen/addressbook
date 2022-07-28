f4f7e9d8d724cdf818519b5cb61c11852ef83ede
Artem Abdullin <artemleelooai@gmail.com>
2022-07-28 15:37:03 +0300

    Added users service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AddressbooksModule } from './addressbooks/addressbooks.module';
 import { GroupsModule } from './groups/groups.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, AddressbooksModule, GroupsModule],
+  imports: [AuthModule, AddressbooksModule, GroupsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
